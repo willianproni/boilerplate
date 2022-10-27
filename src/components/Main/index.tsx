53abfdd26968c696e2cca2d88b35ac5904c3b62a
willianproni <wifepro@hotmail.com>
2022-10-27 14:43:44 -0300

    Add StoryBooks

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Styles from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJs e Styled Components'
+}) => (
   <Styles.Wrapper>
     <Styles.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <Styles.Title> React Avançado </Styles.Title>
-    <Styles.Description>
-      TypeScript, ReactJS, NextJs e Styled Components
-    </Styles.Description>
+    <Styles.Title> {title} </Styles.Title>
+    <Styles.Description>{description}</Styles.Description>
     <Styles.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código"
