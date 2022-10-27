3753268ef9bdda386ec7221800ea580c705e8ec6
willianproni <wifepro@hotmail.com>
2022-10-27 09:52:15 -0300

    Create styles firt component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as Styles from './styles'
+
 const Main = () => (
-  <main>
-    <h1> React Avançado </h1>
-  </main>
+  <Styles.Wrapper>
+    <Styles.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Styles.Title> React Avançado </Styles.Title>
+    <Styles.Description>
+      TypeScript, ReactJS, NextJs e Styled Components
+    </Styles.Description>
+    <Styles.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </Styles.Wrapper>
 )
 
 export default Main
