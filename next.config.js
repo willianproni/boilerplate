9685596c34f6163fd04da63b5a1fd1b16f2699a9
willianproni <wifepro@hotmail.com>
2022-10-28 22:24:27 -0300

    Finish boilerplate

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextConfig = {
   swcMinify: true
 }
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
+
 module.exports = nextConfig
